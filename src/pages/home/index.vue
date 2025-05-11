c14ec694c4c1f25d23c31fd016672fecf4a663f9
qink0043 <3208013769@qq.com>
2025-05-11 22:07:46 +0800

    菜单组件封装

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -240,6 +240,7 @@ onMounted(() => {
 
         .down {
 
+          margin-top: 10px;
           .swiper {
             width: 100%;
             height: 180px;
@@ -250,10 +251,18 @@ onMounted(() => {
               overflow: hidden;
               display: inline-block;
               box-shadow: inset 0 -15px 15px -10px black;
+              box-shadow: 0 0 1px black;
+
+              &:hover img {
+                cursor: pointer;
+                opacity: 0.7;
+                cursor: pointer;
+              }
 
               img {
                 width: 140px;
                 height: 180px;
+                transition: all 0.5s;
               }
 
               &::after {
@@ -283,6 +292,7 @@ onMounted(() => {
                 max-width: 120px;
                 font-size: 12px;
                 z-index: 1;
+                pointer-events: none;
               }
             }
 
